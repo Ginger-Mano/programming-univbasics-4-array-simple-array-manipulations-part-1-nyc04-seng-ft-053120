62b01c102a91f7b17e8fe319c84e292efb9d465a
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-04 23:15:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,15 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array[2].pop
+end
+
+def using_shift(array)
+  array.shift
+end
